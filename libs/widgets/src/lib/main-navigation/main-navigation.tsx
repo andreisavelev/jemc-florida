6414dcb6e3adbc023279dc8a1bb0da6d1137f5b2
Andrei <andrei_savelev@epam.com>
2023-03-16 17:54:25 +0400

    feat: Add Logo

--- libs/widgets/src/lib/main-navigation/main-navigation.tsx
+++ libs/widgets/src/lib/main-navigation/main-navigation.tsx
@@ -3,12 +3,21 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import Link from 'next/link';
+import Image from "next/image";
 
 export function MainNavigation() {
   return (
     <Navbar bg="dark" variant="dark" className="z-3">
       <Container>
-        <Navbar.Brand href="#home">JEMC logo</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <Image
+            src="/images/Jesus_Embassy_Church_Logo.svg"
+            alt="Jesus Embassy Church - Florida"
+            width="48"
+            height="48"
+          />
+          Jesus Embassy Church
+        </Navbar.Brand>
         <Nav className="me-auto">
           <div className="nav-item py-2 py-lg-1 col-12 col-lg-auto">
             <div className="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
