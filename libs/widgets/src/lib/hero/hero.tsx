adc7070d6d716f83df7c508f5025559cb7584fd8
Andrei Savelev <savelevcorr@gmail.com>
2023-03-30 16:40:41 +0400

    Fix format

--- libs/widgets/src/lib/hero/hero.tsx
+++ libs/widgets/src/lib/hero/hero.tsx
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
-import Container from "react-bootstrap/Container";
+import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
-import Row from "react-bootstrap/Row";
-import Button from "react-bootstrap/Button";
-import Link from "next/link";
+import Row from 'react-bootstrap/Row';
+import Link from 'next/link';
 
 const StyledHero = styled.div`
-  background-image: url("/images/hero_pastor.jpeg");
+  background-image: url('/images/hero_pastor.jpeg');
   background-size: cover;
   background-position: center;
   height: 30rem;
@@ -18,7 +17,7 @@ const StyledContainerWrapper = styled.div`
   right: 0;
   bottom: 0;
   left: 0;
-`
+`;
 
 export function Hero() {
   return (
@@ -27,7 +26,9 @@ export function Hero() {
         <Container>
           <Row className="mb-3">
             <Col md={7} className="text-uppercase">
-              <h1 className="display-6">Международный библейский центр «Посольство&nbsp;Иисуса» Флорида</h1>
+              <h1 className="display-6">
+                Международный библейский центр «Посольство&nbsp;Иисуса» Флорида
+              </h1>
             </Col>
           </Row>
           <Row>
